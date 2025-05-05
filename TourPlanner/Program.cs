1435b511c7c078499a7d7eeacab02395462ab142
Barni <if23b200@technikum-wien.at>
2025-05-05 15:12:10 +0200

    Add project files.

---
+++ TourPlanner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TourPlanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
