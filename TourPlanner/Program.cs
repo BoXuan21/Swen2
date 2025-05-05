77cac26668a6b9e4405975214bcb6cd09281f802
bo.yang@rewe-group.at <bo@MacBook-Air-von-Bo.local>
2025-05-05 17:57:34 +0200

    added messages

--- TourPlanner/Program.cs
+++ TourPlanner/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace TourPlanner
+﻿using System;
+using Npgsql; // Installiere das NuGet-Paket "Npgsql" für PostgreSQL-Zugriffe
+
+namespace TourPlanner
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Verbindung zur PostgreSQL-Datenbank
+            string connectionString =
+                "Host=localhost;Database=swen2;Username=postgres;Password=postgres;Include Error Detail=true;";
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Verbindung zur Datenbank erfolgreich!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler beim Herstellen der Verbindung: {ex.Message}");
+            }
         }
     }
 }
