caaf30fbc62c87c4a7de85bba62daf852259dbd2
Barni <if23b200@technikum-wien.at>
2025-05-07 13:31:58 +0200

    Added Maui Windows FE

---
+++ TourPlanner.FrontEnd/TourPlanner.FrontEnd.WinUI/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace TourPlanner.FrontEnd.WinUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
