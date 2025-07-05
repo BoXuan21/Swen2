8276d6830beca4ec6beace2894b22db2fea08163
Baumann Thomas <tbaumann115@gmail.com>
2025-07-05 11:16:19 +0200

    added log4net config and ILogger to Frontend

--- TourPlanner.Frontend/App.xaml.cs
+++ TourPlanner.Frontend/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Reflection;
 using System.Windows;
+using log4net.Config;
+using log4net;
 
 namespace TourPlanner.Frontend
 {
@@ -9,6 +13,13 @@ namespace TourPlanner.Frontend
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
     }
 
 }
